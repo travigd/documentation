e03ab347ec9aa3e7e3ca65d9c4b6c6e69ce5a17b
Dan Leech <hi@danleech.com>
2014-10-22 14:28:13 +0100

    Added menu button

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+    $(".site-navigation-toggle").click(function() {
+        $(".site-navigation").slideToggle();
+    });
     $(".docs-toc-toggle").click(function() {
         $(".docs-sidebar").slideToggle();
     });
