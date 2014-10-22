91ab1e42b8b747f073bbb2ee74fd761446cc33ff
Dan Leech <hi@danleech.com>
2014-10-22 14:30:30 +0100

    Moved JS to main site

--- js/scripts.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $(".site-navigation-toggle").click(function() {
-        $(".site-navigation").slideToggle();
-    });
-    $(".docs-toc-toggle").click(function() {
-        $(".docs-sidebar").slideToggle();
-    });
-});
