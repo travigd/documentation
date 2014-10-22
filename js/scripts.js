47dec1a81e945e1be85a74403369d1ff47c34869
Dan Leech <hi@danleech.com>
2014-10-22 14:24:59 +0100

    Simplified JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
     $(".docs-toc-toggle").click(function() {
-        if($(".docs-sidebar").css("display","none")) {
-            $(".docs-sidebar").slideToggle();
-        }
+        $(".docs-sidebar").slideToggle();
     });
 });
